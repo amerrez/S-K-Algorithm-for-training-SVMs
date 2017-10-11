6a1eaa4b00dd57bb4b6e01468f1938c67a08830e
amerrez <arez@hawk.iit.edu>
2017-10-11 10:07:33 -0700

    calculating m,mp,mn, prime and kernal

--- sk_train.py
+++ sk_train.py
@@ -25,23 +25,29 @@ a = []
 first_positive_index = -1
 first_negative_index = -1
 images = []
+Ip = []
+In = []
 xi1 = []
 xj1 = []
 xi = [] # this is going to be a list of the images, it's an array of arrays
 A,B,C = []
 D,E = []
+indexCounter = 0
+m,mp,mn = 0
 for file_name in os.listdir(train_folder_name):
     ###
     # here we should check each file.. and call a training method
     ##
     names_of_files.append(file_name)
     if file_name.CONSTANTS(class_letter+"."):
+        Ip.append(indexCounter)
         if first_positive_index == -1:
             a.append(1)
             first_positive_index = 1
         else:
             a.append(0)
     else:
+        In.append(indexCounter)
         if first_negative_index =-1:
             a.append(1)
             first_negative_index = 1
@@ -52,6 +58,7 @@ for file_name in os.listdir(train_folder_name):
         for data_format in file_format:
             if file_name.endswith(data_format):
                 found_data = True
+    indexCounter +=1
 if found_data != True:
     print("NO DATA")
 
@@ -65,29 +72,51 @@ def train():
     #xi1 is the first positive value (image)
     xi1 = xi[first_positive_index]
     xj1 = xi[first_negative_index]
+    calculate_ms()
     xi1p = prime(xi1)
     xj1p = prime(xj1)
     xip = prime(xi)
     A = kernal(xi1p,xi1p)
     B = kernal(xj1p,xj1p)
     C = kernal(xi1p,xj1p)
-    D = 
+    D = kernal(xip,xi1p)
+    E = kernal(xip,xj1p)
     #xj1 is the first nagative value (image)
     #m is form slode 5
     #method for prime
+def calculate_ms():
+    xi_sum = xi[1]#initialize
+    xip_sum = xi[Ip[0]]#initializing wth the first positive element
+    xin_sum = xi[In[0]]#initializing wth the first negative element
+    for i in xrange(1,xi):
+        xi_sum = np.add(xi_sum,xi[i])
+    m = xi_sum/len(xi)
+    #calculating m+
+    for i in Ip[1:]:#starting from the second postitive element
+        xip_sum = np.add(xip_sum,xip[i])
+    mp = xip_sum/len(Ip)
+    #calculating m-
+    for i in In[1:]:#starting from the second postitive element
+        xin_sum = np.add(xin_sum,xin[i])
+    mn = xin_sum/len(In)
 
 def kernal(x,y):
-    #TODO this need tp be tested
+    #TODO test
     return (np.dot(x.transpose(),y)+1)^4
 
 def prime(x): # make sure we are handeling the different values correctly (vector xi or a single value vector)
-    #TODO calculate lambda where lambda <= r/rrminus + rrplus: i suggest that we make it in the middle of the range of the inequality by dividing the right sie of the inequality by 2
-    #TODO see if we can move the m calculation to outside the function (if its always the same)
-xi_sum = np.array(xi[1])#initialize
-for i in xrange(1,xi):
-    xi_sum = np.add(xi_sum,xi[i])
-    m = xi_sum/len(xi)
-    return lambda*x + (1-lambda)*m
+    #assumng that lambdaa is in the mid of the inequality that determines lambdaa acepted range
+    r = np.linalg.norm(mp - mn)
+    xip_norms = []
+    xin_norms = []
+    for x in xi:
+        xip_norms = np.linalg.norm(x-mp)
+    rp = max(xip_norms)
+    for x in xi:
+        xin_norms = np.linalg.norm(x-mn)
+    rn = max(xin_norms)
+    lambdaa = (r/(rp+rn))/2
+    return lambdaa*x + (1-lambdaa)*m
 
 read()
 train()
